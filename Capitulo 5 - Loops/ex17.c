7d9173e3adf86de8fa221b9f75f7a82be95956f2
leilatilorenzi-lang <leilatilorenzi@gmail.com>
2025-12-04 13:12:16 -0300

    TEST

--- Capitulo 5 - Loops/ex17.c
+++ Capitulo 5 - Loops/ex17.c
@@ -4,6 +4,7 @@ int main() {
 
     int n_user = 35, total = 1;
     for (int i = 1, j = 1; i < n_user; i++, j++) {
+        
         printf("%d ", i);
 
         if(j == total) {
---
+++ Capitulo 6 - Arrays/exemplo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    float x = 10;
+    printf("x = %f\n", x);
+    x = 20;
+    printf("x = %f\n", x);
+
+    return 0;
+}
